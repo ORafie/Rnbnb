0f6cd3cd83ab505482a49db52d37b5dd9cf35142
Alan Glazer <alancglazer@gmail.com>
2022-12-12 16:29:19 -0600

    added flats controller

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,16 +1,31 @@
 class FlatsController < ApplicationController
   def index
+    @flats = Flat.all
   end
 
   def new
+    @flat = Flat.new
   end
 
   def create
+    @flat = Flat.new(flat_params)
+    if @flat.save
+      redirect_to @flat, notice: 'Flat was successfully created.'
+    else
+      render :new
+    end
   end
 
   def show
+    @flat = Flat.find(params[:id])
   end
 
-  def search
+  # def search
+  # end
+
+  private
+
+  def flat_params
+    params.require(:flat).permit(:title, :description, :price, :capacity) #capacity replaces beds + rooms
   end
 end
