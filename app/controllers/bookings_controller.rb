9d061611677b1f36c3182cb4d4f71bcadba49757
Alan Glazer <alancglazer@gmail.com>
2022-12-12 23:22:34 -0600

    finished bookings controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,5 +1,8 @@
 class BookingsController < ApplicationController
+  before_action :set_booking, only: [:show, :edit, :update]
+
   def new
+    @booking = Booking.new
   end
 
   def show
@@ -9,8 +12,30 @@ class BookingsController < ApplicationController
   end
 
   def create
+    @booking = Booking.new(booking_params)
+
+    if @booking.save
+      redirect_to @booking, notice: 'Your booking was successfully created.'
+    else
+      render :new
+    end
   end
 
   def update
+    if @booking.update(booking_params)
+      redirect_to @booking, notice: 'Your booking was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def set_booking
+    @booking = Booking.find(params[:id])
+  end
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date)
   end
 end
